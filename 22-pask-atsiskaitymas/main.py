ef3a81187c52988cbcc8406e6a92d936d0978fac
PonfoR6 <kryo2005@gmail.com>
2021-11-22 19:14:43 +0200

    22-pask-atsisaitymas
    siek tiek darbelio namie, perdaryta visa programa.

--- 22-pask-atsiskaitymas/main.py
+++ 22-pask-atsiskaitymas/main.py
@@ -1,82 +1,135 @@
-import random
 from tkinter import *
-from tkinter.ttk import *
-
+import random
 import pyperclip
+from tkinter import messagebox
+import json
 
 
-def low():
-    text_box.delete(0, END)
+def random_password():
+    letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    numbers = list('0123456789')
+    symbols = list('!@#$%^&*()_+')
+    letters_lower = list(map(str.lower, letters))
+    letters.extend(letters_lower)
 
-    length = var1.get()
+    num_letters = random.randint(8, 10)
+    num_numbers = random.randint(1, 2)
+    num_symbols = random.randint(1, 2)
 
-    lower = "abcdefghijklmnopqrstuvwxyz"
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()"
-    password = ""
+    random_letters = [random.choice(letters)
+                      for i in range(num_letters)]
+    random_numbers = [random.choice(numbers)
+                      for i in range(num_numbers)]
+    random_symbols = [random.choice(symbols)
+                      for i in range(num_symbols)]
 
-    if var.get() == 1:
-        for i in range(0, length):
-            password = password + random.choice(lower)
-        return password
+    created_password = random_letters + random_numbers + random_symbols
 
-    elif var.get() == 2:
-        for i in range(0, length):
-            password = password + random.choice(upper)
-        return password
+    random.shuffle(created_password)
+    created_password = ''.join(created_password)
 
-    elif var.get() == 3:
-        for i in range(0, length):
-            password = password + random.choice(digits)
-        return password
+    password_entry.delete(0, END)
+    password_entry.insert(0, created_password)
+
+    pyperclip.copy(created_password)
+
+
+def save_pass():
+    user_website = web_entry.get()
+    user_email = email_entry.get()
+    user_password = password_entry.get()
+
+    new_data = {
+        user_website: {
+            'email': user_email,
+            'password': user_password
+        }
+    }
+    if len(user_website) != 0 and len(user_password) != 0 and len(user_email) != 0:
+
+        try:
+            with open("pass.json", "r") as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("pass.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            is_correct = messagebox.askyesno(
+                title=f"{user_website}",
+                message=f"\n'email': {user_email}\n'password': {user_password}\n\nAre you sure?")
+            if is_correct:
+                with open("pass.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+                    web_entry.delete(0, END)
+                    password_entry.delete(0, END)
     else:
-        print("Please choose an option")
+        messagebox.showwarning(
+            title="Error",
+            message="All fields must be filled."
+        )
 
 
-def generate():
-    password1 = low()
-    text_box.insert(10, password1)
+def website_name():
+    user_website = web_entry.get()
+    try:
+        with open("pass.json", "r") as data_file:
+            data = json.load(data_file)
+            user_password = data[user_website]["password"]
+            user_email = data[user_website]["email"]
+            password_entry.delete(0, END)
+            password_entry.insert(0, user_password)
+            email_entry.delete(0, END)
+            email_entry.insert(0, user_email)
+    except KeyError as error_msg:
+        messagebox.showinfo(title="Key Error", message=f"website {error_msg} has no password")
+    except FileNotFoundError as error_msg:
+        messagebox.showinfo(message="Password does not exist.")
 
 
-def copy1():
-    random_password = text_box.get()
-    pyperclip.copy(random_password)
+def login():
+    window2 = Tk()
+    window2.title("Main page")
+    window2.geometry("100x100")
+
+    logged_in = Label(window2, text=" go away ")
+    logged_in.pack()
 
 
 window = Tk()
-window.config(padx=100, pady=100)
-var = IntVar()
-var1 = IntVar()
+window.title("Password Manager")
+window.config(padx=25, pady=50)
 
-window.title("Random Password Generator")
+website_label = Label(text='Website:')
+website_label.grid(row=1, column=0, sticky="W")
 
-random_password = Label(window, text="Password")
-random_password.grid(row=0)
-text_box = Entry(window)
-text_box.grid(row=0, column=1)
+web_entry = Entry(font=("Roboto", 15))
+web_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
 
-length_lbl = Label(window, text="Length")
-length_lbl.grid(row=1)
+lbl_email = Label(text="Email:")
+lbl_email.grid(row=2, column=0, sticky="W")
 
-clipboard = Button(window, text="Copy", command=copy1)
-clipboard.grid(row=1, column=2)
-pass_generator = Button(window, text="Generate", command=generate)
-pass_generator.grid(row=0, column=2)
+password_entry = Entry(font=("Roboto", 15))
+password_entry.grid(row=3, column=1, sticky="EW")
 
-radbtn_low = Radiobutton(window, text="Low", variable=var, value=1)
-radbtn_low.grid(row=2, column=0)
+web_name = Button(text="Search", command=website_name)
+web_name.grid(row=1, column=2, sticky="EW")
 
-radbtn_middle = Radiobutton(window, text="Medium", variable=var, value=2)
-radbtn_middle.grid(row=2, column=1)
+email_entry = Entry(font=("Roboto", 15))
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_entry.insert(0, "email@gmail.com")
 
-radbtn_strong = Radiobutton(window, text="Strong", variable=var, value=3)
-radbtn_strong.grid(row=2, column=2)
+btn_save = Button(text="Add", command=save_pass)
+btn_save.grid(row=4, column=1, columnspan=2, sticky="EW")
+btn_save.config(pady=2)
 
-pass_length = Combobox(window, textvariable=var1)
+lbl_password = Label(text="Password:")
+lbl_password.grid(row=3, column=0, sticky="W")
 
-pass_length['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 20, 25, 30, 35, 40, 45, 50, "Custom length")
-pass_length.current(0)
-pass_length.bind('<<ComboboxSelected>>')
-pass_length.grid(column=1, row=1)
+gen_password = Button(text="Generate Password", command=random_password)
+gen_password.grid(row=3, column=2, sticky="EW")
+
+login_button = Button(text="Login", command=login)
+login_button.grid(row=5, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
